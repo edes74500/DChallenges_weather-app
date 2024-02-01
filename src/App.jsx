3bad073aae03c65f6c7d5265f8877ca1a5db48e1
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2024-02-01 14:13:39 +0100

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { configureStore } from "@reduxjs/toolkit";
 
 //redux
-import { Provider, useDispatch } from "react-redux";
+import { Provider } from "react-redux";
 import rootReducer from "./reducers";
-import { fetchCity } from "./actions/geoLocalisation.action";
 import SearchInput from "./components/searchInput";
 
 const store = configureStore({
@@ -16,8 +12,6 @@ const store = configureStore({
 });
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Provider store={store}>
       <div>
